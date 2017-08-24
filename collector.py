b59e79b23a99aac3508cfabe895a1dbc456bb0e7
Tomer Addady <you@example.com>
2017-08-24 19:00:14 +0300

    add configuration file and writing more to csv

--- collector.py
+++ collector.py
@@ -7,13 +7,9 @@ from pprint import pprint
 from peerplays import PeerPlays
 from peerplays.block import Block
 
-
 def interpetringJson(data1):
     pprint(len(data1["transactions"]))
     pprint((data1["transactions"][0]["operations"][0][1]["from"])) 
-
-
-
 
 def writeTransToCsv(data):
     i = 0 
@@ -59,16 +55,6 @@ def writeTransToCsv(data):
                     alreadyWas.append(int(jsonToBeWrriten['Operations']))
                 i+=1
 
-
-
-
-
-
-
-
-
-
-
 ops = [
         "transfer",  # 0
         "limit_order_create",
@@ -98,54 +84,49 @@ ops = [
         "withdraw_permission_create",
         "withdraw_permission_update",
         "withdraw_permission_claim",
-                                                                                                                        "withdraw_permission_delete",
-                                                                                                                            "committee_member_create",
-                                                                                                                                "committee_member_update",
-                                                                                                                                    "committee_member_update_global_parameters",
-                                                                                                                                        "vesting_balance_create",
-                                                                                                                                            "vesting_balance_withdraw",
-                                                                                                                                                "worker_create",
-                                                                                                                                                    "custom",
-                                                                                                                                                        "assert",
-                                                                                                                                                            "balance_claim",
-                                                                                                                                                                "override_transfer",
-                                                                                                                                                                    "transfer_to_blind",
-                                                                                                                                                                        "blind_transfer",
-                                                                                                                                                                            "transfer_from_blind",
-                                                                                                                                                                                "asset_settle_cancel",
-                                                                                                                                                                                    "asset_claim_fees",
-                                                                                                                                                                                        "fba_distribute",
-                                                                                                                                                                                            "asset_update_dividend",
-                                                                                                                                                                                                "asset_dividend_distribution",
-                                                                                                                                                                                                    "sport_create",
-                                                                                                                                                                                                        "sport_update",
-                                                                                                                                                                                                            "event_group_create",
-    "event_group_update",
+        "withdraw_permission_delete",
+        "committee_member_create",
+        "committee_member_update",
+        "committee_member_update_global_parameters",
+        "vesting_balance_create",
+        "vesting_balance_withdraw",
+        "worker_create",
+        "custom",
+        "assert",
+        "balance_claim",
+        "override_transfer",
+        "transfer_to_blind",
+        "blind_transfer",
+        "transfer_from_blind",
+        "asset_settle_cancel",
+        "asset_claim_fees",
+        "fba_distribute",
+        "asset_update_dividend"
+        "asset_dividend_distribution",
+        "sport_create",
+        "sport_update",
+        "event_group_create",
+        "event_group_update",
         "event_create",
-            "event_update",
-                "betting_market_rules_create",
-                    "betting_market_rules_update",
-                        "betting_market_group_create",
-                            "betting_market_create",
-                                "bet_place",
-                                    "betting_market_group_resolve",
-                                        "betting_market_group_resolved",
-                                            "betting_market_group_freeze",
-                                                "betting_market_group_cancel_unmatched_bets",
-        
+        "event_update",
+        "betting_market_rules_create",
+        "betting_market_rules_update",
+        "betting_market_group_create",
+        "betting_market_create",
+        "bet_place",
+        "betting_market_group_resolve",
+        "betting_market_group_resolved",
+        "betting_market_group_freeze",
+        "betting_market_group_cancel_unmatched_bets",
         "bet_matched",
-                                                        "bet_cancel",
-                                                            "bet_canceled",
-                                                                "tournament_create",
-                                                                    "tournament_join",
-                                                                        "game_move",
-                                                                            "tournament_payout",
-                                                                                "tournament_leave",
-                                                                                ]
-
-
-
-
+        "bet_cancel",
+        "bet_canceled",
+        "tournament_create",
+        "tournament_join",
+        "game_move",
+        "tournament_payout",
+        "tournament_leave",
+    ]
 
 
 """
@@ -189,14 +170,12 @@ if sys.argv[3] == "witness" or sys.argv[3] == "w":
         print ("Block number "+str(startBlockIndex)+" from witness\n")
         startBlockIndex+=1
 
-
 else:
     startBlockIndex = int(sys.argv[1])
     if (sys.argv[2] == "end"):
         endBlockIndex = sys.maxint
     else:
         endBlockIndex = int(sys.argv[2])
-
     titleList = ["BlockNumber", "Operations"]
 #    writerAll = None
     alreadyWas = []
@@ -204,92 +183,16 @@ else:
         writerAll=csv.writer(f)
         writerAll.writerow(titleList)   
     while(startBlockIndex <= endBlockIndex) :
-
         if os.path.isfile('transactions/'+ str(startBlockIndex) +'.json'):    
             data = None
             jsonToBeWrriten = {}
             with open('transactions/'+ str(startBlockIndex) +'.json') as data_file:    
                   data = json.load(data_file)
                   pprint(data)
-
             writeTransToCsv(data)
-            """
-            i = 0 
-            for trans in data["transactions"]:
-                dateTime = data['transactions'][i]['expiration']
-                #typeOf = ""
-                
-                valuesList = [startBlockIndex]
-                for param in cfg["Mapping"]["AllTransaction"]:
-                    exec("jsonToBeWrriten[param]"+ " = data['transactions'][i]" + cfg["Mapping"]["AllTransaction"][param])
-                    valuesList.append(jsonToBeWrriten[param])
-                jsonToBeWrriten["blockNumber"] = startBlockIndex 
-                #valuesList.append(jsonToBeWrriten["blockNumber"])
-                with open('logs/AllTransaction.csv','a') as f:
-                        writer=csv.writer(f)
-                        pprint (valuesList)
-                        writer.writerow(valuesList)
-                pprint (jsonToBeWrriten[param])
-
-                if ops[int(jsonToBeWrriten['Operations'])] in cfg["Mapping"]:
-                    valuesList = [startBlockIndex,dateTime]
-                    titleList = ["BlockNumber", "DateAndTime"]
-                    for param in cfg["Mapping"][ops[int(jsonToBeWrriten['Operations'])]]:
-                        titleList.append(param)
-                        exec("valuesList.append("+ "data['transactions'][i]" + cfg["Mapping"][ops[int(jsonToBeWrriten['Operations'])]][param]+")")
-                    #checks if need to write a new file or to append 
-                    if (int(jsonToBeWrriten['Operations']) in alreadyWas):
-                            with open("logs/" + ops[int(jsonToBeWrriten['Operations'])] + ".csv",'a') as f:
-                                writer=csv.writer(f)
-                                pprint (valuesList)
-                                writer.writerow(valuesList)
-                    else :
-                        with open("logs/" + ops[int(jsonToBeWrriten['Operations'])] + ".csv",'w') as f:
-                            writer=csv.writer(f)
-                            pprint (valuesList)
-                            writer.writerow(titleList)   
-                            writer.writerow(valuesList)  
- 
-                    alreadyWas.append(int(jsonToBeWrriten['Operations']))
-
-                i += 1
-            pprint (jsonToBeWrriten)
-            """
         with open('logs/logFile.log', "a") as logfile:
             logfile.write("Block number "+str(startBlockIndex)+" from directory\n")
         print ("Block number "+str(startBlockIndex)+" from directory\n")
-
         startBlockIndex+=1
 with open('logs/logFile.log', "a") as logfile:
     logfile.write("end of running :)\n")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
